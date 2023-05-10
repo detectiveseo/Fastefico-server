d62b3fa82135acf87d0818380179b51438615e93
detectiveseo <detectiveseo1@gmail.com>
2023-05-10 16:21:27 +0600

    filte filter by chef

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ app.use(cors())
 
 
 const chefs = require('./data/chefs.json')
-app.get('/chef', (req, res) => {
+app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
-app.get('/chef/:id', (req, res) => {
+app.get('/chefs/:id', (req, res) => {
     const id = req.params.id;
     const singleChef = chefs.find(chef => id == chef.id)
     res.send(singleChef)
 })
 
 const foods = require("./data/foods.json")
-app.get("/foods", (req, res) => {
-    res.send(foods)
+app.get("/foods/:chef_name", (req, res) => {
+    const chefName = req.params.chef_name;
+    const recipePerChefe = foods.filter(food => food.chef == chefName)
+    res.send(recipePerChefe)
 })
 
 app.listen(2000, (err) => {
