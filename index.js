6178d3a75127083538d0e9f524e45753992c905a
detectiveseo <detectiveseo1@gmai.com>
2023-08-31 18:00:25 +0600

    backend complete

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = 3000 || process.env.PORT
 app.use(cors())
 
 
 const chefs = require('./data/chefs.json')
+
+app.get("/", (req, res) => {
+    res.send('server is runing on the way')
+})
+
 app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
@@ -14,14 +20,21 @@ app.get('/chefs/:id', (req, res) => {
     res.send(singleChef)
 })
 
+
+
 const foods = require("./data/foods.json")
+
+app.get("/foods", (req, res) => {
+    res.send(foods);
+})
+
 app.get("/foods/:chef_name", (req, res) => {
     const chefName = req.params.chef_name;
     const recipePerChefe = foods.filter(food => food.chef == chefName)
     res.send(recipePerChefe)
 })
 
-app.listen(2000, (err) => {
-    console.log(err)
+app.listen(PORT, (err) => {
+    console.log(`my server is runing at ${PORT}`)
 })
 
